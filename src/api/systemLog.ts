248336b1e12637c1704a1e85bebde8668049df08
WangChong <mz1874@aliyun.com>
2025-06-06 07:15:41 +0800

    # Added system log function

--- src/api/systemLog.ts
+++ src/api/systemLog.ts
@@ -1,1 +1,6 @@
+import axios from "@/api/axios.ts";
 
+export function getSystemLogs(params) {
+    // params = { page: 1, page_size: 10, search: 'admin', ordering: '-operation_time' }
+    return axios.get('/api/system-log/', { params });
+}
